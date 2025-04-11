73525edb263991dd8e4c3f25a8a37664a38e1c05
Anupam2595 <157964451+Anupam2595@users.noreply.github.com>
2025-04-11 20:42:03 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -30,7 +30,7 @@ function Signup() {
         }
         try{
 
-            const url="http://localHost:8088/auth/signup";
+            const url="https://login-project-wine.vercel.app/auth/signup";
             const response=await fetch(url,{
                 method:"POST",
                 headers:{
